ddd11ec5d0ceff8a1ca2c40ece22fc470ae8a000
Alexander Ramm Østgaard <alexostgaard@gmail.com>
2026-01-11 23:08:29 +0100

    Did some work on contacts-page

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,6 +1,12 @@
 import './Contact.css'
 import { useTranslation } from "react-i18next"
 
+import profilePic from "../../assets/pictures/profile.jpeg"
+import githubIcon from "../../assets/contactIcons/github.svg"
+import linkedinIcon from "../../assets/contactIcons/linkedin.svg"
+import instagramIcon from "../../assets/contactIcons/instagram.svg"
+import emailIcon from "../../assets/contactIcons/email.svg"
+
 function Contact() {
     
     const { t } = useTranslation()
@@ -10,9 +16,47 @@ function Contact() {
         <div className="pageContent">
             <div className="pageBackground" id="contactLeft"/>
 
-
+            
             <div className="pageCenter">
-                <h4>{t("maintenance")}</h4>
+
+                <h1>{t("contact.header")}</h1>
+
+                <div className='contactInfoContainer'>
+
+                    <div className='profiles'>
+
+                        <a href='https://github.com/AlexOstgaard'>
+                            <img src={githubIcon} className='contactIcon'/>
+                            <h3>AlexOstgaard</h3>
+                        </a>
+
+                        <a href='https://linkedin.com/in/alexander-ramm-østgaard-19a016306/'>
+                            <img src={linkedinIcon} className='contactIcon'/>
+                            <h3>Alexander Ramm Østgaard</h3>
+                        
+                        </a>
+
+                        <a href='https://instagram.com/alexostgaard/'>
+                            
+                            <img src={instagramIcon} className='contactIcon'/>
+                            <h3>alexostgaard</h3>
+                        
+                        </a>
+
+
+                        <a href='mailto: alexostgaard@gmail.com'>
+                            
+                            <img src={emailIcon} className='contactIcon'/>
+                            <h3>alexostgaard@gmail.com</h3>
+
+                        </a>
+
+                    </div>
+
+                    <img className="profilePic" src={profilePic} />
+
+                </div>
+
             </div>
 
 
